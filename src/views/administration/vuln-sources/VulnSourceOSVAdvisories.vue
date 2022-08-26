d9029f6f90569e865ea04b2361d93cd4981b4904
Sahiba Mittal <sahibamittal98@gmail.com>
2022-08-26 16:50:40 +0100

    WIP

--- src/views/administration/vuln-sources/VulnSourceOSVAdvisories.vue
+++ src/views/administration/vuln-sources/VulnSourceOSVAdvisories.vue
@@ -27,6 +27,7 @@
 <script>
 import { Switch as cSwitch } from '@coreui/vue';
 import common from "../../../shared/common";
+import EcosystemModal from "./EcosystemModal";
 import configPropertyMixin from "../mixins/configPropertyMixin";
 
 export default {
@@ -45,6 +46,82 @@ export default {
         dataOff: '\u2715'
       },
     }
+  },
+  options: {
+    detailFormatter: (index, row) => {
+            return this.vueFormatter({
+              i18n,
+              template: `
+                <b-row class="expanded-row">
+                  <b-col sm="6">
+                    <b-input-group-form-input id="ecosystem-table" :label="$t('admin.team_name')" input-group-size="mb-3"
+                                              required="true" type="text" v-model="name" lazy="true" autofocus="true"
+                                              v-debounce:750ms="updateTeam" :debounce-events="'keyup'" />
+                    <b-form-group :label="this.$t('admin.permissions')">
+                      <div class="list-group">
+                        <span v-for="ecosystem in ecosystems">
+                          <actionable-list-group-item :value="ecosystem" :delete-icon="true" v-on:actionClicked="removePermission(permission)"/>
+                        </span>
+                        <actionable-list-group-item :add-icon="true" v-on:actionClicked="$root.$emit('bv::show::modal', 'selectPermissionModal')"/>
+                      </div>
+                    </b-form-group>
+                  </b-col>
+                  <select-permission-modal v-on:selection="updatePermissionSelection" />
+                </b-row>
+              `,
+              mixins: [],
+              components: {
+                cSwitch,
+                ActionableListGroupItem,
+                SelectPermissionModal,
+                BInputGroupFormInput
+              },
+              data() {
+                return {
+                  team: row,
+                  name: row.name,
+                  permissions: row.permissions,
+                  labelIcon: {
+                    dataOn: '\u2713',
+                    dataOff: '\u2715'
+                  }
+                }
+              },
+              methods: {
+                
+                updatePermissionSelection: function(selections) {
+                  this.$root.$emit('bv::hide::modal', 'selectPermissionModal');
+                  for (let i=0; i<selections.length; i++) {
+                    let selection = selections[i];
+                    let url = `${this.$api.BASE_URL}/${this.$api.URL_PERMISSION}/${selection.name}/team/${this.team.uuid}`;
+                    this.axios.post(url
+                    ).then((response) => {
+                      this.syncVariables(response.data);
+                      this.$toastr.s(this.$t('message.updated'));
+                    }).catch((error) => {
+                      if (error.response.status === 304) {
+                        //this.$toastr.w(this.$t('condition.unsuccessful_action'));
+                      } else {
+                        this.$toastr.w(this.$t('condition.unsuccessful_action'));
+                      }
+                    });
+                  }
+                },
+                removePermission: function(permission) {
+                  let url = `${this.$api.BASE_URL}/${this.$api.URL_PERMISSION}/${permission.name}/team/${this.team.uuid}`;
+                  this.axios.delete(url).then((response) => {
+                    this.syncVariables(response.data);
+                    this.$toastr.s(this.$t('message.updated'));
+                  }).catch((error) => {
+                    this.$toastr.w(this.$t('condition.unsuccessful_action'));
+                  });
+                },
+                syncVariables: function(team) {
+                  this.permissions = team.permissions;
+                }
+              }
+            })
+          },
   },
   methods: {
     saveChanges: function() {
